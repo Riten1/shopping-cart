29ad7c8c5f55d789c1604ff3df9e435ad8ad9b40
Riten1 <122423532+Riten1@users.noreply.github.com>
2024-03-28 13:46:29 +0545

    'setLoader'

--- project/src/components/header/Header.jsx
+++ project/src/components/header/Header.jsx
@@ -1,35 +1,30 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-        <nav className='flex items-center justify-between h-20 max-w-6xl mx-auto'>
-          <Link to='/'>
-            <div>
-              <h1 className='text-red-900 font-bold text-xl sm:text-2xl md:text-3xl cursor-pointer tracking-wide decoration-none'>React Shopping Cart</h1>
-            </div>
-          </Link>
+      <nav className="flex items-center justify-between h-20 max-w-6xl mx-auto">
+        <Link to="/">
+          <div>
+            <h1 className="text-red-900 font-bold text-xl sm:text-2xl md:text-3xl cursor-pointer tracking-wide decoration-none">
+              React Shopping Cart
+            </h1>
+          </div>
+        </Link>
 
-          <ul className='flex list-none items-center space-x-6 text-gray-800 font-semibold'>
-          <li className='cursor-pointer'>
-          <Link to='/'>Home
-          </Link>
+        <ul className="flex list-none items-center space-x-6 text-gray-800 font-semibold">
+          <li className="cursor-pointer">
+            <Link to="/">Home</Link>
           </li>
-        
-          <li className='cursor-pointer'>
-          <Link to='/cart'>Cart
-          </Link>
+
+          <li className="cursor-pointer">
+            <Link to="/cart">Cart</Link>
           </li>
-         
-          </ul>
-          
-        
-        </nav>
-      
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- project/src/pages/Home.jsx
+++ project/src/pages/Home.jsx
@@ -1,33 +1,47 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Circles } from "react-loader-spinner";
 
 const Home = () => {
- const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-async function fetchProducts(){
-  const res = await fetch('https://fakestoreapi.com/products')
-  const data = await res.json()
-  console.log(data)
-  setProducts(data)
-}
+  async function fetchProducts() {
+    setLoading(true);
+    const res = await fetch("https://fakestoreapi.com/products");
+    const data = await res.json();
+    console.log(data);
 
- useEffect(() => {
-  try {
-    fetchProducts()
-  } catch (error) {
-    console.log(error)
+    if (data) {
+      setLoading(false);
+      setProducts(data);
+    }
   }
-  
 
- },[])
+  useEffect(() => {
+    try {
+      fetchProducts();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
-    <div>
-      {products.map((item) => (
-        <div>{item.title}</div>
-      ))}
-    </div>
-  )
-}
+    <>
+      {loading ? (
+        <div className="min-h-screen w-full flex justify-center items-center">
+          <Circles
+            height={"120"}
+            width={"120"}
+            color="rgb(127, 29, 29)"
+            visible={true}
+          />
+        </div>
+      ) : (
+        products.map((item) => <div key={item.id}>{item.title}</div>)
+      )}
+    </>
+  );
+};
 
-export default Home
+export default Home;
